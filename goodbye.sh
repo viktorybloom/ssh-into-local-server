e5ff9f35e32da8ac191f612b281be74f9ad8da62
viktor <viktor@winkreports.com>
2024-01-14 21:49:18 +1000

    remove handshake from server ~/.ssh/authorized_devices

--- goodbye.sh
+++ goodbye.sh
@@ -6,12 +6,17 @@ read -p "Enter your server hostname or IP address: " HOSTNAME
 read -sp "Enter your server password: " PASSWORD
 echo  # Move to a new line after password input
 
-# Step 1: Remove SSH Handshake from /etc/ssh/sshd_config
+# Step 1: Remove SSH Handshake from /etc/ssh/ssh_config 
 echo "Removing SSH handshake from /etc/ssh/ssh_$USERNAME@$HOSTNAME..."
 ssh "$USERNAME@$HOSTNAME" "sudo sed -i '/$USERNAME@$HOSTNAME/d' /etc/ssh/ssh_config"
 echo "SSH handshake removed from /etc/ssh/ssh_$USERNAME@$HOSTNAME on the server."
 
-# Step 2: Restart SSH Service
+# Step 2: Remove authorized keys for the user
+echo "Removing authorized keys for $USERNAME on $HOSTNAME..."
+ssh "$USERNAME@$HOSTNAME" "echo > ~/.ssh/authorized_keys"
+echo "Authorized keys removed for $USERNAME on $HOSTNAME."
+
+# Step 3: Restart SSH Service
 echo "Restarting SSH service on the server..."
 ssh "$USERNAME@$HOSTNAME" "sudo systemctl restart ssh"
 echo "SSH service restarted on the server."
